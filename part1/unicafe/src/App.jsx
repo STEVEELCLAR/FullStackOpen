f3b76ad3a14e804faa19554fb7645b497ac910a2
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-03-10 14:20:46 +0800

    unicafe-Step2
    Expanded the application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+  
+  const setGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
+    setAverage((updatedGood - bad)/(updatedGood + neutral + bad))
+    setPercentage(updatedGood/(updatedGood + neutral + bad)*100)
+  }
+  const setNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+    setAverage((good - bad)/(good + updatedNeutral + bad))
+    setPercentage(good/(good + updatedNeutral + bad)*100)
+  }
+  const setBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
+    setAverage((good - updatedBad)/(good + neutral + updatedBad))
+    setPercentage((good/(good + neutral + updatedBad))*100)
+  }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={setGoodClick}>good</button>
+      <button onClick={setNeutralClick}>neutral</button>
+      <button onClick={setBadClick}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {percentage} %</p>
+    </div>
+  )
+}
+
+export default App
