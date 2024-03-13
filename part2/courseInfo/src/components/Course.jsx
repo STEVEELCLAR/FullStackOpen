d34bac64a94be90a193a7ceb7b5ec5b7eb7c77c3
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-03-13 17:02:16 +0800

    courseInfor-Step7
    -modified the App.jsx because I forgot to implement: The application must work regardless of the number of parts a course has, so make sure the application works if you add or remove parts of a course.
    -also added the part that the application must show also the sum of the exercises of the course.

--- part2/courseInfo/src/components/Course.jsx
+++ part2/courseInfo/src/components/Course.jsx
@@ -1,31 +1,32 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ parts }) => {
+    let sum = 0
+    parts.map((part) => {
+        sum += part.exercises
+    })
+    console.log(sum)
+    return (
+        <b>total of {sum} exercises</b>
+    )
+}
+
+
 
 const Part = ({ part }) => 
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Content = ({ parts }) => parts.map((part,) => <Part key={part.id } part= {part} />)
+
 
 const Course = ({course}) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> */}
+      <Total parts={course.parts} />
     </div>
   )
 }
