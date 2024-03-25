01d7df843911ca4bf8a771b08acfc72865034650
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-03-25 16:12:14 +0800

    phoneBook-Step3
    Expand the application by allowing users to add phone numbers to the phone book. Created useState and event handler for the new number input and also adjusted the DisplayName component to display the number.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,45 @@
 import { useState } from 'react'
 
-const DisplayName = ({name}) => <p> {name} </p>
+const DisplayName = ({name, number}) => <p> {name} {number} </p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '092304943'
+    }
   ])
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addNote = (event) => {
     event.preventDefault()
     console.log
   
 
+     if (persons.find((person) => person.name === newName )){
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
+     }
+    
+    else{
+      const nameObject = {
+        name: newName,
+        number: newNumber
+      }
+    
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -27,6 +50,9 @@ const App = () => {
           name: <input value = {newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value = {newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -34,7 +60,7 @@ const App = () => {
       <div>
       {persons.map((person, index) => (
           <div key={index}>
-            <DisplayName name={person.name} />
+            <DisplayName name={person.name} number = {person.number}/>
           </div>
         ))}
     </div>
