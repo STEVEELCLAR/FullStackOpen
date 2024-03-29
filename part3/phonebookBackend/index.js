ab243ace1c7c409859999a00e210d652e30076b3
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-03-29 16:11:11 +0800

    phonebookBackend-Step8
    Configure morgan so that it also shows the data sent in HTTP POST requests

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+
+morgan.token('req-body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body', {
+  skip: (req, res) => req.method !== 'POST' 
+}));
+
 let persons = [
     { 
       "id": 1,
