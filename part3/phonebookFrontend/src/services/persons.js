8fa8f2706a3ec8c1bc65d4fda3b8aa7c7024366a
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-03-30 21:53:38 +0800

    phonebookDatabase-Step1
    Change the fetching of all phonebook entries so that the data is fetched from the database.
    
    -Make a copy of the phonebookBackend to the part3 directory and change its name to phonebookDatabase.
    -modify the index.js by addign the mongoDB

--- part3/phonebookFrontend/src/services/persons.js
+++ part3/phonebookFrontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://fullstackopen-5gbr.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
