934dbce8e3bc19648a8075acbc7ca163d3478c4b
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-04-02 23:22:30 +0800

    helperFunction&UnitTest-Step2and3
    -Define a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts.
    
    -Define a new favoriteBlog function that receives a list of blogs as a parameter. The function finds out which blog has the most likes. If there are many top favorites, it is enough to return one of them.

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,7 +1,26 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  }
+
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((previous, current)=>(previous.likes > current.likes ? previous : current ))
+    return {
+            title: favorite.title,
+            author: favorite.author,
+            likes: favorite.likes
+          
+    }
+}
+
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
