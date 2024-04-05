94f8d5bcf5c3fb65c8900f7608c49e59d32990a4
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-04-06 00:23:38 +0800

    blogListTest-Step3
    4.10: Blog List Tests, step 3
    Write a test that verifies that making an HTTP POST request to the /api/blogs URL successfully creates a new blog post. At the very least, verify that the total number of blogs in the system is increased by one. You can also verify that the content of the blog post is saved correctly to the database.
    
    Once the test is finished, refactor the operation to use async/await instead of promises.

---
+++ part4/blogListTests/tests.js/totalNumberofBlogs.test.js
@@ -0,0 +1,57 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const assert = require('node:assert')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+const initialblogs = [
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+    },
+    {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+    }  
+  ]
+test('blogs are saved correctly', async () => {
+  const repsonse = await api
+    .post('/api/blogs')
+    .send(initialblogs[2])
+    .expect(200)
+  
+  const blogCount = await Blog.find({}).exec()
+  assert.strictEqual(blogCount.length>2, true)
+
+  const savedBlog = await Blog.find({title: initialblogs[2].title}).exec()
+  assert.ok(savedBlog.length > 0, "Saved Blog exists")
+  assert.strictEqual(savedBlog[0].author, initialblogs[2].author)
+  assert.strictEqual(savedBlog[0].title, initialblogs[2].title)
+  assert.strictEqual(savedBlog[0].url, initialblogs[2].url)
+  assert.strictEqual(savedBlog[0].likes, initialblogs[2].likes)
+
+})
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialblogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialblogs[1])
+    await blogObject.save()
+  })
+
+after(async () => {
+  await mongoose.connection.close()
+})
