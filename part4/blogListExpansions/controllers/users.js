25087101f12fc9dcc9fb53d99abeeb03d71d7124
Steve Clar <42365522+STEVEELCLAR@users.noreply.github.com>
2024-04-07 16:20:29 +0800

    blogListExpansions-Step4
    Add a feature which adds the following restrictions to creating new users: Both username and password must be given and both must be at least 3 characters long. The username must be unique.
    
    but modified the contoroller/user.js because the code in the example does not work for me.

--- part4/blogListExpansions/controllers/users.js
+++ part4/blogListExpansions/controllers/users.js
@@ -2,7 +2,8 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
+  try{
   const { username, name, password } = request.body
 
   const saltRounds = 10
@@ -17,6 +18,9 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+} catch (error){
+  next(error)
+}
 })
 
 module.exports = usersRouter
--- part4/blogListExpansions/utils/list_helper.js
+++ part4/blogListExpansions/utils/list_helper.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const User = require('../models/user')
 
 const usersInDb = async () => {
